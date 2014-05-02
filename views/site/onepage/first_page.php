5e826830cd559405fbccf01c472fbdc05476371a
Philipp Frenzel <philipp@frenzel.net>
2014-05-02 19:53:16 +0200

    remove bull shit slider

--- views/site/onepage/first_page.php
+++ views/site/onepage/first_page.php
@@ -36,8 +36,20 @@
         </div>
     </div>
 </div>
+
+<?php
+
+?>
+
 <div class="slide" data-anchor="slide2" id="slide2">
     <div class="container">
-        
+        <div class="slider single-item">
+            <div><h3>1</h3></div>
+            <div><h3>2</h3></div>
+            <div><h3>3</h3></div>
+            <div><h3>4</h3></div>
+            <div><h3>5</h3></div>
+            <div><h3>6</h3></div>
+        </div>
     </div>
 </div>
