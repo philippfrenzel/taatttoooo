cbc281b48976ae3fccc0fb912cf7da9dea9ebade
Philipp Frenzel <philipp@frenzel.net>
2014-05-02 17:15:28 +0200

    update font

--- views/site/onepage/first_page.php
+++ views/site/onepage/first_page.php
@@ -17,13 +17,12 @@
     <div class="col-lg-4">
         <h2>Show us your Tattoo!</h2>
 
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-            ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-            fugiat nulla pariatur.</p>
+        <p>We care about body art and so we would like to share your style and thoughts. Pls. post us your
+            pictures in "RAW"-format and we'll prepare a "set" for you. None tattoo will be published without
+            your aggreament of what our "photoshopers" made out of it!</p>
     </div>
     <div class="col-lg-4">
-        <h2>And tell us your Story!</h2>
+        <h2>And tell us the Story!</h2>
 
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
             dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
