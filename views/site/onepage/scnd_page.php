590e213a222c4e48baec697f6d9b63a546c05776
Philipp Frenzel <philipp@frenzel.net>
2014-05-03 15:52:54 +0200

    fix pager

--- views/site/onepage/scnd_page.php
+++ views/site/onepage/scnd_page.php
@@ -1,40 +1,38 @@
-<div class="slide" data-anchor="slide21" id="slide21">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6">
-                <h1 class="ultrabig">About Us!</h1>
-            </div>
-            <div class="col-md-6">
-                
-            </div>
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <h1 class="ultrabig">About Us!</h1>
         </div>
+        <div class="col-md-6">
+            
+        </div>
+    </div>
 
-       <div class="row">
-           &nbsp;
-       </div>
+   <div class="row">
+       &nbsp;
+   </div>
 
-        <div class="row">
-            <div class="col-lg-4">
-            </div>
-            <div class="col-lg-4">
-                <h2>Where we from...</h2>
+    <div class="row">
+        <div class="col-lg-4">
+        </div>
+        <div class="col-lg-4">
+            <h2>Where we from...</h2>
 
-                <p class="bg-color-darken opacity">
-                    Our headoffice is located in Vienna - Austria. Where we life, study, party and sleep.
-                    We stands for our design team, web coding devision and a lot of wonderfull friends who 
-                    support us. If you wanna join one of them, let us know! webmaster (at) taatttoooo com will
-                    be your point of interest.
-                </p>
-            </div>
-            <div class="col-lg-4">
-                <h2>What we do...</h2>
+            <p class="bg-color-darken opacity">
+                Our headoffice is located in Vienna - Austria. Where we life, study, party and sleep.
+                We stands for our design team, web coding devision and a lot of wonderfull friends who 
+                support us. If you wanna join one of them, let us know! webmaster (at) taatttoooo com will
+                be your point of interest.
+            </p>
+        </div>
+        <div class="col-lg-4">
+            <h2>What we do...</h2>
 
-                <p class="bg-color-darken opacity">
-                    We are specialised on online solution development and marketing. Building adorable new
-                    solutions to share with the community is one of our main targets. So the taatttoooo page
-                    has been setup to take care of one of the other main targets which is named FUN!
-                </p>
-            </div>
+            <p class="bg-color-darken opacity">
+                We are specialised on online solution development and marketing. Building adorable new
+                solutions to share with the community is one of our main targets. So the taatttoooo page
+                has been setup to take care of one of the other main targets which is named FUN!
+            </p>
         </div>
     </div>
 </div>
