0c15cd82f59fb360a72dc265db693bb0ae695945
Philipp Frenzel <philipp@frenzel.net>
2014-05-04 19:49:38 +0200

    fix spacing to bottom

--- views/site/onepage/scnd_page.php
+++ views/site/onepage/scnd_page.php
@@ -13,9 +13,6 @@
         </div>
     </div>
     <div class="row">
-        <h2>&nbsp;</h2>
-    </div>
-    <div class="row">
         <div class="col-md-6"></div>
         <div class="col-md-4 center-block">
             <i class="fa fa-chevron-down fa-5x"></i>                
