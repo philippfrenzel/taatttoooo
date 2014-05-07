88d6b7363cb2e13eeafc214fd3b7e9ab7697cf06
Philipp Frenzel <philipp@frenzel.net>
2014-05-07 22:15:12 +0200

    hiu

--- views/story/_form.php
+++ views/story/_form.php
@@ -20,22 +20,22 @@ use yii\helpers\Url;
     ]); ?>
 
     <div class="row">
-        <div class="col-md-6">
-            <?= $form->field($model, 'content_tattoo')->textarea(['rows' => 3]) ?>
-        </div>
-        <div class="col-md-6">
-            <?= $form->field($model, 'content_past')->textarea(['rows' => 3]) ?>
+        <div class="col-md-12">
+            <?= $form->field($model, 'content_tattoo')->textarea(['rows' => 7]) ?>
         </div>
     </div>
 
-    <div class="row">
+    <!--div class="row">
         <div class="col-md-6">
-            <?= $form->field($model, 'content_present')->textarea(['rows' => 3]) ?>
+            <?php//= $form->field($model, 'content_past')->textarea(['rows' => 3]) ?>
         </div>
         <div class="col-md-6">
-            <?= $form->field($model, 'content_future')->textarea(['rows' => 3]) ?>
+            <?php//= $form->field($model, 'content_present')->textarea(['rows' => 3]) ?>
         </div>
-    </div>
+        <div class="col-md-6">
+            <?php//= $form->field($model, 'content_future')->textarea(['rows' => 3]) ?>
+        </div>
+    </div/-->
 
     <?= Html::activeHiddenInput($model,'uId'); ?>
 
