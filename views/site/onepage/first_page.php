0654a35ce2073edded6392ab1ade14c2a8dd93b2
Philipp Frenzel <philipp@frenzel.net>
2014-05-07 21:20:31 +0200

    update text

--- views/site/onepage/first_page.php
+++ views/site/onepage/first_page.php
@@ -1,46 +1,60 @@
 <div class="slide" data-anchor="slide1" id="slide1">
     <div class="container">
         <div class="row">
-            <div class="col-lg-4">
-            </div>
-            <div class="col-lg-4">
-                <h2>Do you have:</h2>
+            <div class="col-lg-7">
+                <div class="row">
+                    <div class="col-md-4">
+                        
+                    </div>
+                    <div class="col-md-8">
+                        <h2>Do you have:</h2>
 
-                <div class="bg-color-darken opacity">
-                    <h4>A favorite place?</h4>
-                    <p>
-                        It can be a restaurant
-                    </p>
+                        <div class="bg-color-darken opacity">
+                            <h4>A favorite place?</h4>
+                            <p>
+                                It can be a restaurant, a street, or just a place in a park or the middle of nowhere. 
+                                Something you think it unique to Texas and that you’d like to share with others.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    &nbsp;
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <h1 class="ultrabig">Are you reading this from Texas ?</h1>
+                        <h3 class="subultra">(or are you from there ?)</h3>
+                    </div>
                 </div>
             </div>
-            <div class="col-lg-4">
-                <h2>And tell us the Story!</h2>
+            <div class="col-lg-5">
+                <h2>A tattoo about Texas?</h2>
+                <div class="bg-color-darken opacity">
+                    <p>
+                        I’ve never seen a place with more proud people and proud tattoos of where they’re from than Texas. 
+                        If you have one, and want to share, let us know! 
+                    </p>
+                </div>
 
-                <p class="bg-color-darken opacity">
-                    As you matter! Pls. tell us the Story that belongs to your tattoo, what it means to you and 
-                    share your thoughts about your life. If you like, let us know about your past, the present and
-                    your future!
-                </p>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-6">
-                <h1 class="ultrabig">Are you reading this from Texas ?</h1>
-                <h3 class="subultra">(or are you from there ?)</h3>
-            </div>
-            <div class="col-md-2">
-                
-            </div>
-            <div class="col-md-4">
-<?php
-    if(class_exists('app\widgets\PortletUserStory'))
-    {
-        echo app\widgets\PortletUserStory::widget([
-            'title' => NULL
-        ]);
-    }
-?>
-            </div>
+                <h2>Anything else you’d like to share?</h2>
+
+                <div class="bg-color-darken opacity">
+                    <p>
+                        A song, a quote, a memory. If you know others who might want to contribute, 
+                        share with them too!  
+                    </p>
+                </div>
+
+                <?php
+                    if(class_exists('app\widgets\PortletUserStory'))
+                    {
+                        echo app\widgets\PortletUserStory::widget([
+                            'title' => NULL
+                        ]);
+                    }
+                ?>
+            </div>        
         </div>
         <div class="row">
             <h2>&nbsp;</h2>
--- views/site/onepage/thrd_page.php
+++ views/site/onepage/thrd_page.php
@@ -1,24 +1,47 @@
 <div class="container">
     <div class="row">
-        <div class="col-lg-4">
+        <div class="col-lg-3">
+        </div>
+        <div class="col-lg-5">
+            <h2>Who we are:</h2>
+
+            <div class="bg-color-darken opacity">
+                <h4>Cassandra</h4>
+                <p>
+                    I was born in Houston and grew up in Fredericksburg, TX. 
+                    I was a cheerleader as a kid, ran for one of those crazy local pageants (Peach Queen), 
+                    went to almost every county fair, and floated rivers more times than I can count, 
+                    in the nearly 20 years I lived and grew up there.
+                    Now I live part time in Paris, Vienna, and more and more rarely these days, Texas. 
+                    The Texas necklace on the other page is my own that I’ve had since 
+                    I first moved away for school.
+                </p>
+                <h4>Philipp</h4>
+                <p>
+                    I was born in Stuttgart (Germany) and grew up half time in the city and half time on the 
+                    country sight. I made a lot of sports, exspecialy Tennis! While working hard on the sand
+                    courts I'll always loved programming and creating arts on the computer. I although love to paint
+                    real pictures;) Since 6 years I'm living in Vienna - working as a BI consultant - and now getting the chance to visit Texas,
+                    where I think the most adorable girl I know got raised. 
+                </p>
+
+            </div>
         </div>
         <div class="col-lg-4">
-            <h2>Where we from...</h2>
+            <h2>The Project</h2>
 
             <p class="bg-color-darken opacity">
-                Our headoffice is located in Vienna - Austria. Where we live, study, party and sleep.
-                We stands for our design team, web coding devision and a lot of wonderfull friends who 
-                support us. If you wanna join one of them, let us know! webmaster (at) taatttoooo com will
-                be your point of interest.
-            </p>
-        </div>
-        <div class="col-lg-4">
-            <h2>What we do...</h2>
-
-            <p class="bg-color-darken opacity">
-                We are specialised on online solution development and marketing. Building adorable new
-                solutions to share with the community is one of our main targets. So the taatttoooo page
-                has been setup to take care of one of the other main targets which is named FUN!
+                I’ve always been interested in how proud Texans are of their home, 
+                how beautiful so much of the state is, and the so many ways that people show 
+                their love of our state. Since I’ve moved away, 
+                I’ve been asked some of the craziest questions about Texas, 
+                seen countless friends move there and start families of their own there, 
+                and had a great time sharing food and traditions from growing up.
+                I thought this book would be a great way to highlight some of the things 
+                I know and love about home. It obviously won’t tell the story of how everyone 
+                feels about Texas. There’s 26-something million of you in the state alone not even 
+                counting those of us who have moved, but it can tell some of those stories. 
+                And I hope it will be a great one you want to share. 
             </p>
         </div>
     </div>
