a3210d67db30be5c40db013f223f17e520ac64fa
Philipp Frenzel <philipp@frenzel.net>
2014-05-07 22:25:46 +0200

    hiou

--- widgets/views/iviews/_view_story.php
+++ widgets/views/iviews/_view_story.php
@@ -8,7 +8,7 @@ use yii\helpers\HtmlPurifier;
 
 <div class="postbox bg-color-darken opacity">
   <div class="teaseimg pull-left">
-    <img src="<?= Url::to(['/dmsys/getlatestthumb','id'=>$model->id,'module'=>1]); ?>" style="height:25px" alt="thumb"/>
+    <img src="<?= Url::to(['/dmsys/getlatestthumb','id'=>$model->id,'module'=>1]); ?>" style="height:125px" alt="thumb"/>
   </div>
   <div>
     <p>
